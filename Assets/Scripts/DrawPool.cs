f3577360ceda96913abbdd7db3fbce106030c9dd
Jose Eduardo - NPC <joeduardo_almeidajr@hotmail.com>
2015-07-02 17:18:55 -0300

    Inventory e Luz Adicioinado Signed-off-by: Jose Eduardo - NPC <joeduardo_almeidajr@hotmail.com>

--- Assets/Scripts/DrawPool.cs
+++ Assets/Scripts/DrawPool.cs
@@ -7,42 +7,83 @@ public class DrawPool : MonoBehaviour {
 	private Transform instantiatedPool;
 	public bool PaintWithClickMouse = false;
 	public float DestroyIn = 5F;
+	public Light DirLight;
+	private bool lighting = false;
 
-	public Animator animator;
-	bool isStunned;
+	private Animator animator;
+	public bool isStunned;
+
+	//Warrior types
+	public enum Warrior{Karate, Ninja, Brute, Sorceress};
+	
+	public Warrior warrior;
 
 	// Use this for initialization
 	void Start () {
-	
+		animator = GetComponent<Animator> ();
+		//DirLight = GetComponent<Light> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (PaintWithClickMouse) {
-			if (!Input.GetMouseButton (0)) {
-				return;
-			}
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-		
-			RaycastHit hit;
-			if (Physics.Raycast (ray, out hit, Mathf.Infinity)) {
-				Paint (hit.point, Quaternion.LookRotation (hit.normal), color); // Step back a little
-			}
-		} else {
-			if (Input.GetButtonDown("Fire1"))
-			{
-				animator.SetTrigger("Attack1Trigger");
-				if (warrior == Warrior.Brute)
-					StartCoroutine (COStunPause(1.2f));
-				else if (warrior == Warrior.Sorceress)
-					StartCoroutine (COStunPause(1.2f));
-				else
-					StartCoroutine (COStunPause(.6f));
-
-				Paint (transform.position, transform.rotation, color); // Step back a little					
+		if(Input.GetKeyDown( KeyCode.I )){
+			if(isStunned){
+				isStunned = false;
+			}else{
+				isStunned = true;
 			}
 		}
 
+		if (!isStunned) {
+			if (PaintWithClickMouse) {
+				if (!Input.GetMouseButton (0)) {
+					return;
+				}
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		
+				RaycastHit hit;
+				if (Physics.Raycast (ray, out hit, Mathf.Infinity)) {
+					Paint (hit.point, Quaternion.LookRotation (hit.normal), color); // Step back a little
+				}
+			} else {
+				if (Input.GetButtonDown ("Fire1")) {
+					animator.SetTrigger ("Attack1Trigger");
+					if (warrior == Warrior.Brute)
+						StartCoroutine (COStunPause (1.2f));
+					else if (warrior == Warrior.Sorceress)
+						StartCoroutine (COStunPause (1.2f));
+					else
+						StartCoroutine (COStunPause (.6f));
+
+					StartCoroutine (COSPaint (.5f));
+				}
+
+				// AUMENTAR A LUZ QUE ENVOLVE O PERSONAGEM
+				if (Input.GetButtonDown ("Fire2")) {
+					isStunned = true;
+					lighting = true;
+				}
+
+			}
+		}else{
+			//CONTROLE PARA AUMENTAR A LUZ QUE ENVOLVE O PERSONAGEM
+			if(lighting){
+				if(DirLight.range <= 250){
+					DirLight.range += 5;
+				}else{
+					DirLight.range = 10;
+					isStunned = false;
+					lighting = false;
+				}
+			}
+		}
+
+	}
+
+	public IEnumerator COSPaint(float pauseTime)
+	{
+		yield return new WaitForSeconds(pauseTime);
+		Paint (transform.position, transform.rotation, color);
 	}
 
 	public IEnumerator COStunPause(float pauseTime)
--- Assets/Warrior Pack Bundle 1 Free/Code/WarriorAnimationDemo.cs
+++ Assets/Warrior Pack Bundle 1 Free/Code/WarriorAnimationDemo.cs
@@ -8,61 +8,46 @@ public class WarriorAnimationDemo : MonoBehaviour {
 	float rotationSpeed = 30;
 	Vector3 inputVec;
 	bool isMoving;
-	bool isStunned;
 
+	private DrawPool DPool;
 
 	//Warrior types
 	public enum Warrior{Karate, Ninja, Brute, Sorceress};
 
 	public Warrior warrior;
-	
+
+	void Start () {
+		animator = GetComponent<Animator> ();
+		DPool = GetComponent<DrawPool> ();
+	}
+
 	void Update()
 	{
-		//Get input from controls
-		float x = -(Input.GetAxisRaw("Horizontal"));
-		float z = -(Input.GetAxisRaw("Vertical"));
-		inputVec = new Vector3(x, 0, z);
+		if (!DPool.isStunned) {
+			//Get input from controls
+			float x = -(Input.GetAxisRaw ("Horizontal"));
+			float z = -(Input.GetAxisRaw ("Vertical"));
+			inputVec = new Vector3 (x, 0, z);
 
-		//Apply inputs to animator
-		animator.SetFloat("Input X", z);
-		animator.SetFloat("Input Z", -(x));
+			//Apply inputs to animator
+			animator.SetFloat ("Input X", z);
+			animator.SetFloat ("Input Z", -(x));
 
-		if (x != 0 || z != 0 )  //if there is some input
-		{
-			//set that character is moving
-			animator.SetBool("Moving", true);
-			isMoving = true;
-			animator.SetBool("Running", true);
+			if (x != 0 || z != 0) {  //if there is some input
+				//set that character is moving
+				animator.SetBool ("Moving", true);
+				isMoving = true;
+				animator.SetBool ("Running", true);
+			} else {
+				//character is not moving
+				animator.SetBool ("Moving", false);
+				animator.SetBool ("Running", false);
+				isMoving = false;
+			}
+			UpdateMovement ();  //update character position and facing
 		}
-		else
-		{
-			//character is not moving
-			animator.SetBool("Moving", false);
-			animator.SetBool("Running", false);
-			isMoving = false;
-		}
-/*
-		if (Input.GetButtonDown("Fire1"))
-		{
-			animator.SetTrigger("Attack1Trigger");
-			if (warrior == Warrior.Brute)
-				StartCoroutine (COStunPause(1.2f));
-			else if (warrior == Warrior.Sorceress)
-				StartCoroutine (COStunPause(1.2f));
-			else
-				StartCoroutine (COStunPause(.6f));
-		}
-*/
-		UpdateMovement();  //update character position and facing
 	}
-
-	public IEnumerator COStunPause(float pauseTime)
-	{
-		isStunned = true;
-		yield return new WaitForSeconds(pauseTime);
-		isStunned = false;
-	}
-	
+		
 	void RotateTowardsMovementDir()  //face character along input direction
 	{
 		if (inputVec != Vector3.zero)
@@ -83,18 +68,4 @@ public class WarriorAnimationDemo : MonoBehaviour {
 		return inputVec.magnitude;  //return a movement value for the animator, not currently used
 	}
 
-	void OnGUI () 
-	{
-		/*
-		if (GUI.Button (new Rect (25, 85, 100, 30), "Attack1")) 
-		{
-			animator.SetTrigger("Attack1Trigger");
-
-			if (warrior == Warrior.Brute || warrior == Warrior.Sorceress)  //if character is Brute or Sorceress
-				StartCoroutine (COStunPause(1.2f));
-			else
-				StartCoroutine (COStunPause(.6f));
-		}
-		*/
-	}
 }
